51e0bd50747ba3f48d841e09b2b43a9a5717bc23
Angela Luo <ael49021@gmail.com>
2020-02-28 17:13:13 -0600

    formatted code

--- src/components/AssignmentPage/AssignmentPage.js
+++ src/components/AssignmentPage/AssignmentPage.js
@@ -3,18 +3,22 @@ import CalendarComponent from "../CalendarComponent/CalendarComponent";
 import { withFirebase } from "utils/Firebase";
 import { compose } from "recompose";
 function AssignmentPage() {
-    const [Date, setDate] = useState()
-    function handleCalendarChange(value) {
-        setDate(value)
-    }
-    return (
-      <div style={{
-        position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-        }}>
-        <CalendarComponent handleChange = {handleCalendarChange}/>
-      </div>
-    );
+  const [Date, setDate] = useState();
+  function handleCalendarChange(value) {
+    setDate(value);
   }
-  
-  export default compose(withFirebase)(AssignmentPage);
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)"
+      }}
+    >
+      <CalendarComponent handleChange={handleCalendarChange} />
+    </div>
+  );
+}
+
+export default compose(withFirebase)(AssignmentPage);
--- src/components/CalendarComponent/CalendarComponent.js
+++ src/components/CalendarComponent/CalendarComponent.js
@@ -1,24 +1,24 @@
 import React, { useState, setState, useEffect } from "react";
-import Calendar from 'react-calendar/dist/entry.nostyle'
-import 'CalendarComponent/CalendarComponent.css'
+import Calendar from "react-calendar/dist/entry.nostyle";
+import "CalendarComponent/CalendarComponent.css";
 import { withFirebase } from "utils/Firebase";
-function CalendarComponent (props) {
-  function onChange (date) {
-    props.handleChange({date})
+function CalendarComponent(props) {
+  function onChange(date) {
+    props.handleChange({ date });
   }
 
   return (
-      <div>
-      <Calendar 
-      onClickDay={event => {
-        onChange(event)
-      } }
-      defaultValue = {new Date()}
-      className = "calendar"
-      calendarType = "US" 
-      minDetail = "month"
+    <div>
+      <Calendar
+        onClickDay={event => {
+          onChange(event);
+        }}
+        defaultValue={new Date()}
+        className="calendar"
+        calendarType="US"
+        minDetail="month"
       />
-      </div>
-  )
+    </div>
+  );
 }
-export default withFirebase(CalendarComponent)
+export default withFirebase(CalendarComponent);
