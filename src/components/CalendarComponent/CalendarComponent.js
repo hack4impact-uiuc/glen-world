e048ccea268ba98523b68ce05552364855e72699
Angela Luo <ael49021@gmail.com>
2020-02-27 23:38:29 -0600

    Changed color and size of calendar component

---
+++ src/components/CalendarComponent/CalendarComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Calendar from 'react-calendar/dist/entry.nostyle'
+import 'CalendarComponent/CalendarComponent.css'
+//import 'react-calendar/dist/Calendar.css'
+// import { Calendar} from 'antd';
+import {Jumbotron} from 'react-bootstrap';
+import 'antd/dist/antd.css'
+class CalendarComponent extends React.Component {
+  onPanelChange(value, mode) {
+    console.log(value, mode);
+  }
+  render() {
+  return (
+    <div style={{ color: '#1DA57A', width: 290, border: '1px solid #d9d9d9', borderRadius: 4 }}>
+
+      <Calendar calendarType = "US" 
+      className = "calendar"
+      defaultView = 'month'
+      style = {{
+        background: '#DD98FD'
+      }}
+        />
+    </div>
+  )
+  }
+}
+export default CalendarComponent
