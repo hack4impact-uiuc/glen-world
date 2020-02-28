9715bcf0b16fc59a77297ea0e4ad6509688a4f44
Angela Luo <ael49021@gmail.com>
2020-02-28 15:10:28 -0600

    CalendarComponent stores date as Thu Feb 06 2020 in this.state.date

--- src/components/CalendarComponent/CalendarComponent.js
+++ src/components/CalendarComponent/CalendarComponent.js
@@ -1,27 +1,25 @@
 import React from "react";
 import Calendar from 'react-calendar/dist/entry.nostyle'
 import 'CalendarComponent/CalendarComponent.css'
-//import 'react-calendar/dist/Calendar.css'
-// import { Calendar} from 'antd';
-import {Jumbotron} from 'react-bootstrap';
-import 'antd/dist/antd.css'
 class CalendarComponent extends React.Component {
-  onPanelChange(value, mode) {
-    console.log(value, mode);
+  state = {
+    date: new Date(),
+  }
+  onChange = date => {
+    this.setState({ date })
+    console.log(this.state.date)
   }
   render() {
   return (
-    <div style={{ color: '#1DA57A', width: 290, border: '1px solid #d9d9d9', borderRadius: 4 }}>
-
-      <Calendar calendarType = "US" 
-      minDetail = "month"
-      nextAriaLabel = "potato"
+      <div>
+      <Calendar 
+      onChange={this.onChange}
+      value={this.state.date}
       className = "calendar"
-      style = {{
-        background: '#DD98FD'
-      }}
-        />
-    </div>
+      calendarType = "US" 
+      minDetail = "month"
+      />
+      </div>
   )
   }
 }
