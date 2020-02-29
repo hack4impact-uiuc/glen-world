5ef178e43bc86db3770424196d319e32515f4269
Angela Luo <ael49021@gmail.com>
2020-02-29 13:34:44 -0600

    fix positioning of calendar component

--- src/components/AssignmentPage/AssignmentPage.js
+++ src/components/AssignmentPage/AssignmentPage.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import CalendarComponent from "../CalendarComponent/CalendarComponent";
+import { Container, Row, Col } from "react-bootstrap";
 import { withFirebase } from "utils/Firebase";
 import { compose } from "recompose";
+import "bootstrap/dist/css/bootstrap.min.css";
 function AssignmentPage() {
   const [Date, setDate] = useState();
   function handleCalendarChange(value) {
@@ -16,7 +18,16 @@ function AssignmentPage() {
         transform: "translate(-50%, -50%)"
       }}
     >
-      <CalendarComponent handleChange={handleCalendarChange} />
+      <Container>
+        <Row>
+          <Col>
+            <div>Insert Student List component</div>
+          </Col>
+          <Col>
+            <CalendarComponent handleChange={handleCalendarChange} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
