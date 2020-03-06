2ede59e7cf9d6d339e9d1a4a80cf2ec52ab7523f
Angela Luo <ael49021@gmail.com>
2020-03-05 23:48:04 -0600

    functionality is working

--- src/components/AssignmentPage/AssignmentPage.js
+++ src/components/AssignmentPage/AssignmentPage.js
@@ -8,15 +8,56 @@ import DatePicker from "../DatePicker/DatePicker";
 import StudentList from "../StudentList/StudentList";
 function AssignmentPage() {
   const [Date, setDate] = useState();
+  const [DeploymentAccounts, setDeploymentAccounts] = useState([]);
+
+  const json = [
+    { "deploymentId": "potatopotatopotato",
+       "deploymentAccounts": [ 
+           "curly fries",
+           "waffle fries",
+           "home fries",
+           "tator tots",
+           "smiley fries",
+           "crinkly fries"
+       ]
+     }, 
+     { "deploymentId": "lamtalkssomuchohmygod",
+       "deploymentAccounts": [ 
+           "genedidntbringgummybears",
+           "kellyisdaddy",
+           "weshouldgetpatagonias",
+           "roy: no minecraft server or patagonias", 
+           "anisha: your design sucks"
+       ]
+     },
+     { "deploymentId": "imsotired",
+       "deploymentAccounts": [ 
+           "thisshitissostupid",
+           "imswitchingtobackend",
+           "fuckjavascript",
+           "wtfisevengoingon", 
+           "idkhowtofrontend:(imjustgoodatcopyingandpasting"
+       ]
+     },
+    ]
   function handleDatePickerChange(value) {
     setDate(value);
   }
+  function handleDeploymentAccounts(value) {
+    setDeploymentAccounts(value);
+  }
+
   return (
     <div className="place_middle">
+      {console.log(DeploymentAccounts)}
+      {console.log(Date)}
       <Container>
         <Row>
+          <h1>this is the assignment page yall</h1>
+        </Row>
+        <Row>
           <Col>
-            <StudentList/>
+            <StudentList deployments = {json} handleChange = {handleDeploymentAccounts}/>
           </Col>
           <Col>
             <DatePicker handleChange={handleDatePickerChange} />
--- src/components/StudentList/StudentList.js
+++ src/components/StudentList/StudentList.js
@@ -1,13 +1,11 @@
 import React, { useState, setState, useEffect } from "react";
 import { withFirebase } from "utils/Firebase";
 import PropTypes from 'prop-types';
-import List from '@material-ui/core/List';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import { makeStyles } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import {StylesProvider, Checkbox, ListItemIcon, Collapse, List, ListSubheader, ListItem, ListItemText} from '@material-ui/core';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import { FixedSizeList } from 'react-window';
-
 import 'StudentList/StudentList.css';
 
 const useStyles = makeStyles(theme => ({
@@ -17,34 +15,80 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
     position: 'relative',
     overflow: 'auto',
-    maxHeight: 300,
   },
   listSection: {
-    backgroundColor: 'inherit',
+    backgroundColor: '#00d8a4',
+    color: '#e6e6e6',
   },
   ul: {
-    backgroundColor: 'inherit',
+    backgroundColor: '#e7e4e4',
     padding: 0,
+  },
+  nested: {
+    backgroundColor: '#7f7eff',
+    color: '#e6e6e6',
+    paddingLeft: theme.spacing(4),
   },
 }));
 
-export default function StudentList() {
+export default function StudentList(props) {
   const classes = useStyles();
+  const [checked, setChecked] = React.useState([0]);
+  const [open, setOpen] = React.useState(Array(props.deployments.length).fill().map((_, i) => false));
+  let deployments = props.deployments
+
+  const handleClick = (index)  => {
+    let openCopy = [...open];
+    openCopy[index] = !openCopy[index]
+    setOpen(openCopy)
+  }
+
+  const handleToggle = value => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+    props.handleChange(newChecked);
+  };
 
   return (
-    <List className={classes.root} subheader={<li />}>
-      {[0, 1, 2, 3, 4].map(sectionId => (
-        <li key={`section-${sectionId}`} className={classes.listSection}>
-          <ul className={classes.ul}>
-            <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-            {[0, 1, 2].map(item => (
-              <ListItem key={`item-${sectionId}-${item}`}>
-                <ListItemText primary={`Item ${item}`} />
-              </ListItem>
+    <div>
+      <List>
+      {deployments.map((deployment, index) => (   
+        <div>  
+            
+            <ListItem button onClick={() => handleClick(index)} className= {classes.listSection}>
+              {`Class: ${deployment["deploymentId"]}`} 
+              {open[index] ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+
+            <Collapse in={open[index]} timeout="auto" component = 'ListItem' node = 'ListItem' unmountOnExit>
+            {deployment["deploymentAccounts"].map((student, open)=> ( 
+              <ListItem 
+              button onClick={handleToggle(student)}
+              className= {classes.nested}
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    tabIndex={-1}
+                    checked={checked.indexOf(student) !== -1}
+                    disableRipple
+                  />
+                </ListItemIcon>
+                <ListItemText  primary={`${student}`} />
+              </ListItem>            
             ))}
-          </ul>
-        </li>
+            </Collapse>
+          </div>
       ))}
     </List>
+    </div>
   );
 }
