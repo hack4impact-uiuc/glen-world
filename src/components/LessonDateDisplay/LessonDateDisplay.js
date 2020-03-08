1fd52e19199f3fb689231ed10b693b1bd2993f1d
Prashant Pokhriyal <prashant.pokhriyal09@gmail.com>
2020-03-08 15:43:28 -0500

    Switched to just using indices

--- src/components/LessonDateDisplay/LessonDateDisplay.js
+++ src/components/LessonDateDisplay/LessonDateDisplay.js
@@ -2,26 +2,9 @@ import React, { useState, useEffect } from "react";
 import "./LessonDateDisplay.css";
 
 function LessonDateDisplay(props) {
-  const [seconds, setSeconds] = useState(null);
-
-  useEffect(() => {
-    setSeconds(props.date["seconds"]);
-  }, []);
-
-  function toDateTime(secs) {
-    var t = new Date(1970, 0, 1); // Epoch
-    t.setSeconds(secs);
-    return t;
-  }
-
   return (
     <div className="DateIcon">
-      <p className="Date">
-        {Intl.DateTimeFormat("en-US", {
-          month: "short",
-          day: "2-digit"
-        }).format(toDateTime(seconds).getMonth())}
-      </p>
+      <p className="Date">{props.number}</p>
     </div>
   );
 }
