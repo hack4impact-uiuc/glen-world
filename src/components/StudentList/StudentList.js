9312f56587e22a948bb3251b47a9045042e88bb0
Angela Luo <42478474+AngelaLuo49021@users.noreply.github.com>
2020-03-07 22:11:03 -0600

    Student list component (#19)
    * Create a customLessons endpoint that returns all the custom lessons created by an admin.
    
    * yarn format everything
    
    * Parse result from customLessons endpoint in ExampleComponent
    
    * Changed color and size of calendar component
    
    * disabled year/century view
    
    * CalendarComponent stores date as Thu Feb 06 2020 in this.state.date
    
    * Export CalendarComponent with firebase
    
    * Create an Assignment Page to hold the Calendar Component
    
    * formatted code
    
    * fix positioning of calendar component
    
    * Rename the CalendarComponent to DatePicker
    
    * Found a list component from mbreact
    
    * functionality is working
    
    * changed styling
    
    * finished Student List Component and added to Assignment Page
    
    * formatted everything
    
    * fixed changes thatkelley requested:

---
+++ src/components/StudentList/StudentList.js
@@ -0,0 +1,89 @@
+import React, { useState, setState, useEffect } from "react";
+import { withFirebase } from "utils/Firebase";
+import {
+  Checkbox,
+  ListItemIcon,
+  Collapse,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import useStyles from "StudentList/StudentListStyles.js";
+
+function StudentList(props) {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([0]);
+  const [open, setOpen] = React.useState(
+    Array(props.deployments.length)
+      .fill()
+      .map((_, i) => false)
+  );
+  let deployments = props.deployments;
+
+  const handleClick = index => {
+    let openCopy = [...open];
+    openCopy[index] = !openCopy[index];
+    setOpen(openCopy);
+  };
+
+  const handleToggle = value => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+    props.handleChange(newChecked);
+  };
+  return (
+    <div>
+      <List>
+        {Array.isArray(deployments) &&
+          deployments.map((deployment, index) => (
+            <div>
+              <ListItem
+                button
+                onClick={() => handleClick(index)}
+                className={classes.listSection}
+              >
+                {`Class: ${index + 1}`}
+                {open[index] ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+
+              <Collapse in={open[index]} timeout="auto" unmountOnExit>
+                {Array.isArray(deployment["deploymentAccounts"]) &&
+                  deployment["deploymentAccounts"].map(student => (
+                    <ListItem
+                      className={classes.nested}
+                      button
+                      onClick={handleToggle(student)}
+                    >
+                      <ListItemIcon>
+                        <Checkbox
+                          classes={{
+                            root: classes.root,
+                            checked: classes.checked
+                          }}
+                          edge="start"
+                          tabIndex={-1}
+                          checked={checked.indexOf(student) !== -1}
+                          disableRipple
+                        />
+                      </ListItemIcon>
+                      <ListItemText primary={`${student["username"]}`} />
+                    </ListItem>
+                  ))}
+              </Collapse>
+            </div>
+          ))}
+      </List>
+    </div>
+  );
+}
+export default withFirebase(StudentList);
--- src/pages/AssignmentPage/AssignmentPage.js
+++ src/pages/AssignmentPage/AssignmentPage.js
@@ -1,35 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { withFirebase } from "utils/Firebase";
 import { compose } from "recompose";
 import "bootstrap/dist/css/bootstrap.min.css";
-import DatePicker from "../../components/DatePicker/DatePicker";
-import WordGroupSelector from "../../components/GroupSelector/WordGroupSelector";
-
-function AssignmentPage() {
-  const [Words, setWords] = useState([]);
+import StudentList from "components/StudentList/StudentList";
+import DatePicker from "components/DatePicker/DatePicker.js";
+import getDeploymentAccountsFromAdmin from "utils/Firebase/firebase.js";
+function AssignmentPage({ firebase }) {
   const [Date, setDate] = useState();
-  function handleWordSelectorChange(value) {
-    setWords(value);
-  }
+  const [DeploymentAccounts, setDeploymentAccounts] = useState([]);
+  const [AdminDeployments, setAdminDeployments] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .getDeploymentAccountsFromAdmin("q9SKXnmXunTooHg9yokcB9vKiZt2")
+      .then(deploymentAccounts => {
+        setAdminDeployments(deploymentAccounts);
+      });
+  }, []);
+
   function handleDatePickerChange(value) {
     setDate(value);
   }
+  function handleDeploymentAccounts(value) {
+    setDeploymentAccounts(value);
+  }
   return (
-    <div>
-      <WordGroupSelector handleChange={handleWordSelectorChange} />
-      <div className="place_middle">
-        <Container>
-          <Row>
-            <Col>
-              <div>Insert Student List component</div>
-            </Col>
-            <Col>
-              <DatePicker handleChange={handleDatePickerChange} />
-            </Col>
-          </Row>
-        </Container>
-      </div>
+    <div className="place_middle">
+      <Container>
+        <Row>
+          <h1>this is the assignment page yall</h1>
+        </Row>
+        <Row>
+          <Col>
+            <StudentList
+              deployments={AdminDeployments}
+              handleChange={handleDeploymentAccounts}
+            />
+          </Col>
+          <Col>
+            <DatePicker handleChange={handleDatePickerChange} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
