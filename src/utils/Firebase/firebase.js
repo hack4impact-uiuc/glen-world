2ecc16dbb4c291a7366774a50fc88649ba87cdab
Lam Tran <Lam7150@gmail.com>
2020-03-07 20:05:02 -0600

    Created all FETCH and POST endpoints (#18)
    * finishing getLastMastered endpoint
    
    * finishing endpoints: getLastMasteredLesson, getCustomLessons, getStudentSummaries
    
    * formatting
    
    * added comments
    
    * created getStudentSummaries and getStudentCustomLesson PULL endpoints
    
    * you're welcome Angela
    
    * i love you daddy
    
    * finishing fixes and creating examples
    
    * fuck formatting
    
    Co-authored-by: gwang0136 <genew2@illinois.edu>

--- src/utils/Firebase/firebase.js
+++ src/utils/Firebase/firebase.js
@@ -22,11 +22,6 @@ class Firebase {
 
     this.googleProvider = new app.auth.GoogleAuthProvider();
   }
-  //Data access APIs
-  customLessons = adminAccountId =>
-    this.db
-      .collection(`custom_lesson`)
-      .where("adminAccountId", "==", adminAccountId);
 
   doCreateUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
@@ -36,23 +31,153 @@ class Firebase {
 
   doSignOut = () => this.auth.signOut();
 
-  // EXAMPLE ENDPOINT
-  getLesson = () => this.db.collection("custom_lesson");
-
-  //Gets last mastered lesson of a given student.
-  getLastMastered = deploymentAccountId =>
+  // Gets the most recent lesson that a student has completed
+  getLastMasteredLesson = deploymentAccountId =>
     this.db
       .doc(`deployment_account/${deploymentAccountId}/`)
       .get()
-      .then(documentSnapshot => {
-        documentSnapshot.get("profile.glenLearn.lastMasteredLesson");
-      });
+      .then(deploymentAccountDoc => {
+        const lastMasteredLesson = deploymentAccountDoc.get(
+          "profile.glenLearn.lastMasteredLesson"
+        );
+        return new Promise((resolve, reject) => {
+          if (deploymentAccountDoc) {
+            resolve(lastMasteredLesson);
+          } else {
+            reject(Error("Deployment doc does not exist"));
+          }
+        });
+      })
+      .catch(error => console.log("Error getting student account: ", error));
 
-  //Data access APIs
-  customLessons = adminAccountId =>
+  // Gets custom lessons made by admin
+  getAdminCustomLessons = adminAccountId =>
     this.db
       .collection(`custom_lesson`)
-      .where("adminAccountId", "==", adminAccountId);
+      .where("adminAccountId", "==", adminAccountId)
+      .get()
+      .then(lessonQuerySnapshot => {
+        const customLessons = lessonQuerySnapshot.docs.map(doc => doc.data());
+        return new Promise((resolve, reject) => {
+          if (lessonQuerySnapshot) {
+            resolve(customLessons);
+          } else {
+            reject(Error("No custom lessons found."));
+          }
+        });
+      })
+      .catch(error => console.log("Error getting custom lessons: ", error));
+
+  // Get student's assigned custom lessons
+  getDeploymentAccountCustomLessons = deploymentAccountId =>
+    this.db
+      .doc(`deployment_account/${deploymentAccountId}/`)
+      .get()
+      .then(deploymentAccountDoc => {
+        const customLessonIds = deploymentAccountDoc.get("customLessons");
+        const customLessonRefs = customLessonIds.map(id =>
+          this.db.doc(`custom_lesson/${id}`).get()
+        );
+
+        return Promise.all(customLessonRefs)
+          .then(customLessonDocs => {
+            const customLessons = customLessonDocs.map(doc => doc.data());
+            return customLessons;
+          })
+          .catch(error =>
+            console.log("Error getting all custom lessons: ", error)
+          );
+      })
+      .catch(error => console.log("Error getting student account: ", error));
+
+  // Get students of admin
+  getDeploymentAccountsFromAdmin = adminAccountId =>
+    this.db
+      .doc(`admin_account/${adminAccountId}/`)
+      .get()
+      .then(adminDoc => {
+        const deploymentIds = Object.keys(adminDoc.get("deployments"));
+        const deploymentRefs = deploymentIds.map(id =>
+          this.db
+            .collection(`deployment_account`)
+            .where("deploymentId", "==", id)
+        );
+
+        return Promise.all(deploymentRefs)
+          .then(deploymentQuery => {
+            let deployments = [];
+            for (var i = 0; i < deploymentIds.length; i++) {
+              let deployment = {
+                deploymentId: deploymentIds[i],
+                deploymentAccounts: {}
+              };
+
+              // Getting deployment accounts from deploymentId query
+              deploymentQuery[i]
+                .get()
+                .then(querySnapshot => {
+                  deployment.deploymentAccounts = querySnapshot.docs.map(doc =>
+                    doc.data()
+                  );
+                })
+                .catch(error =>
+                  console.log("Error gettng all deployments: ", error)
+                );
+
+              deployments.push(deployment);
+            }
+            return deployments;
+          })
+          .catch(error =>
+            console.log("Error getting all deployment refs: ", error)
+          );
+      })
+      .catch(error => console.log("Error getting admin account: ", error));
+
+  addCustomLesson = (
+    adminAccountId,
+    deploymentAccountIds,
+    lessonTemplate,
+    wordGroup,
+    words,
+    dueDate
+  ) => {
+    // Push specific custom lesson to admin account
+    let customLessonRef = this.db.collection("custom_lesson").doc();
+
+    customLessonRef
+      .set({
+        adminAccountId: adminAccountId,
+        deploymentAccountIds: deploymentAccountIds,
+        lessonTemplate: lessonTemplate,
+        wordGroup: wordGroup,
+        words: words,
+        dueDate: dueDate
+      })
+      .catch(error => console.log("Error creating custom lesson: ", error));
+
+    // TODO: needs error validation to stop function if customLesson fails
+
+    // Push custom lessons to deployments
+    let batch = this.db.batch();
+    let deploymentAccountId;
+
+    for (deploymentAccountId of deploymentAccountIds) {
+      let deploymentRef = this.db.doc(
+        `deployment_account/${deploymentAccountId}/`
+      );
+      batch.update(deploymentRef, {
+        customLessons: app.firestore.FieldValue.arrayUnion(customLessonRef.id)
+      });
+    }
+
+    return batch
+      .commit()
+      .catch(error =>
+        console.log("Pushing custom lessons to deployments failed: ", error)
+      );
+    // needs better promise rejection
+  };
 }
 
 export default Firebase;
