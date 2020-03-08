28adb7e6d52e283b848eb49c169a21532ef88ed9
Lam <Lam7150@gmail.com>
2020-03-08 15:45:47 -0500

    fixing date component

--- src/components/LessonDateDisplay/LessonDateDisplay.js
+++ src/components/LessonDateDisplay/LessonDateDisplay.js
@@ -2,26 +2,18 @@ import React, { useState, useEffect } from "react";
 import "./LessonDateDisplay.css";
 
 function LessonDateDisplay(props) {
-  const [seconds, setSeconds] = useState(null);
-
-  useEffect(() => {
-    setSeconds(props.date["seconds"]);
-  }, []);
-
-  function toDateTime(secs) {
-    var t = new Date(1970, 0, 1); // Epoch
-    t.setSeconds(secs);
-    return t;
+  function toDateTime() {
+    let dateComponents = props.date
+      .toDate()
+      .toDateString()
+      .split(" ");
+    let dateString = dateComponents[1].concat(" ", dateComponents[2]);
+    return dateString;
   }
 
   return (
     <div className="DateIcon">
-      <p className="Date">
-        {Intl.DateTimeFormat("en-US", {
-          month: "short",
-          day: "2-digit"
-        }).format(toDateTime(seconds).getMonth())}
-      </p>
+      <p className="Date">{toDateTime()}</p>
     </div>
   );
 }
