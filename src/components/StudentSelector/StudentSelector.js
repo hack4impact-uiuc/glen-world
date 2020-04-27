eb6fad265b58c12955e02fbc836c2f6c11e3a040
Angela Luo <ael49021@gmail.com>
2020-04-27 18:46:21 -0500

    added select all button, no functionality yet

--- src/components/StudentSelector/StudentSelector.js
+++ src/components/StudentSelector/StudentSelector.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { DropdownButton, Dropdown } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 import { withFirebase } from "utils/Firebase";
 import "./StudentSelector.scss"
 
 function StudentSelector(props) {
     const [currentDeployment, setCurrentDeployment] = useState()
+    const [studentsChecked, setStudentsChecked] = useState([]);
     let deployments = props.deployments;
 return (
     <div className = "student-selector">
@@ -18,6 +20,20 @@ return (
                 </Dropdown>
             ))}
         </DropdownButton>
+        <div className = "select-all-checkbox">
+            <Row>
+            <div className = "select-all-margins">
+            <label className = "select-all-label">Select All</label>  
+            </div>
+            <div>
+            <input
+                class = "move-down"
+                id = "cbox"
+                type = "checkbox"
+            />
+            </div>
+            </Row>    
+        </div>
         
     </div>
 )
