27a06f33d980e10d0b664c2b6e9f4880b4af7ae3
Angela Luo <ael49021@gmail.com>
2020-05-04 23:24:58 -0500

    WIP of fixing the lesson cards display

--- src/components/LessonCardsDisplay/LessonCardsDisplay.js
+++ src/components/LessonCardsDisplay/LessonCardsDisplay.js
@@ -1,28 +1,35 @@
 import React from "react";
 import "./LessonCardsDisplay.scss";
+import { Col, Row, Container } from "reactstrap";
 import LessonCard from "../LessonCards/LessonCard";
 
 function LessonCardsDisplay(props) {
   return (
     <div className="LessonCardsDisplay">
+      <Row>
+      <Col>
       <div onClick={props.addCard}>
         <div className="DefaultCard">
           Click to assign students to a selected date!
         </div>
       </div>
+      </Col>
       {Object.keys(props.cards).length > 0 &&
         Object.keys(props.cards)
           .reverse()
           .map(date => {
             const dateTemp = date;
             return (
+              <Col>
               <LessonCard
                 lessonDate={dateTemp}
                 lessonStudents={props.cards[date][1]}
                 deleteCard={props.removeCard}
               />
+              </Col>
             );
           })}
+      </Row>
     </div>
   );
 }
